72950789c9136d55aa3d51eed5f19583a7c95f74
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-08-29 17:35:07 +0000

    same30

--- pointers_gone_wild/part2-main.c
+++ pointers_gone_wild/part2-main.c
@@ -32,7 +32,7 @@ int main() {
 
 
   char *strange = malloc(32);
-  strange = strcat("0\0\0\0\0", "5");
+  strange = "0\0\0\0\0 5";
   printf("%d", *(int *)(strange + 5));
   strange_step(strange);
   free(strange);
