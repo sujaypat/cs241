236ca4edd13c64ffbbdc1e82d132ea9e6de8e4bd
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-01 20:37:32 +0000

    init commit

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -1,8 +1,43 @@
 /**
- * Chatroom Lab
- * CS 241 - Fall 2016
- */
+* Chatroom Lab
+* CS 241 - Fall 2016
+*/
 #include "camelCaser.h"
 #include <stdlib.h>
 
-char **camel_caser(const char *input_str) { return NULL; }
+char **camel_caser(const char *input_str) { return NULL;
+	char *buf = NULL;
+	char ** res;
+	char* delims = "!&+:?^}\"\',;@_~#(-<[`$).=\\{%%*/>]|";
+	// size_t linecap = 0;
+	// ssize_t linelen;
+	// while((linelen = getline(&buf, &linecap, &input_str)) > 0){
+	//
+	// }
+
+	int c = 0;
+	int i = 0;
+	int numStrings = 0;
+	while(c != -1){
+		// c = fgetc(input_str);
+		c = getdelim(&buf, &linecap, delims, input_str);
+		strcpy(res[i++], buf);
+
+		// if((isalpha(c) || isdigit(c))){
+		// 	strcat(buf, c);
+		// }
+		// else if((isspace(c) || ispunct(c))){
+		// 	*res[numStrings++] = buf;
+		// 	buf = NULL;
+		// }
+		// else{
+		// 	// probably NULL
+		// }
+	}
+	for(int a = 0; a < i; a++){
+		prinft("%s\n",res[a]);
+	}
+
+
+
+}
