16c2435d428988edd5fac66abd834ecc170e083f
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-02 15:30:16 +0000

    thanks46

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -22,7 +22,7 @@ char **camel_caser(const char *input_str) {
 
 		if(ispunct(*input_str)){
 			res[c] = calloc(i + 1, sizeof(char));
-			*(res[c] + 1) = "\0";
+			strncpy(res[c], "\0", i+1);
 			strncat(res[c], input_beg, i);
 
 			c++;
