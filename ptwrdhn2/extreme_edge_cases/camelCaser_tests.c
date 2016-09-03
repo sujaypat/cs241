693da7b7e2cb149919fbc3ab776227cb1994e905
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-03 03:23:28 +0000

    thanks71

--- ptwrdhn2/extreme_edge_cases/camelCaser_tests.c
+++ ptwrdhn2/extreme_edge_cases/camelCaser_tests.c
@@ -1,7 +1,7 @@
 /**
- * Chatroom Lab
- * CS 241 - Fall 2016
- */
+* Chatroom Lab
+* CS 241 - Fall 2016
+*/
 #include "camelCaser_tests.h"
 
 #include <stdio.h>
@@ -11,19 +11,34 @@
 #include "camelCaser.h"
 
 /*
- * Testing function for various implementations of camelCaser.
- *
- * @param  camelCaser   A pointer to the target camelCaser function.
- * @return              Correctness of the program (0 for wrong, 1 for correct).
- */
+* Testing function for various implementations of camelCaser.
+*
+* @param  camelCaser   A pointer to the target camelCaser function.
+* @return              Correctness of the program (0 for wrong, 1 for correct).
+*/
 int test_camelCaser(char **(*camelCaser)(const char *)) {
-  // TODO: Return 1 if the passed in function works properly; 0 if it doesn't.
-  char *input = "ayy lmao. same tbh";
-  char *correct = "ayyLmao";
-  int result = 0;
-  char **res = (*camelCaser)(input);
-  printf("%s\n", res[0]);
-  result = !strcmp(correct, res[0]);
-  return result;
-  // use assert
+	// TODO: Return 1 if the passed in function works properly; 0 if it doesn't.
+	// char *input = "ayy lmao. same tbh";
+	// char *correct = "ayyLmao";
+	// int result = 0;
+	// char **res = (*camelCaser)(input);
+	// printf("res is supposed to be: %s\n", res[0]);
+	// result = !strcmp(correct, res[0]);
+	// return result;
+	char * inputs[] = {
+		" ",
+		"ayy lmao",
+		"Hello World.",
+		"The Heisenbug is an incredible creature. Facenovel servers get their power from its indeterminism. Code smell can be ignored with INCREDIBLE use of air freshener. God objects are the new religion.",
+		NULL
+	};
+
+	char ** input = inputs;
+	while(*input){
+		print_input(*input);
+		char **output = (*camelCaser)(*input);
+		print_output(output);
+		input++;
+	}
+	// use assert
 }
