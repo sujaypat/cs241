f70c1aa8e406a35922f0a712351a87088a9ece8e
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-04 20:04:22 +0000

    thanks149

--- extreme_edge_cases/reference/camelCaser_main.c
+++ extreme_edge_cases/reference/camelCaser_main.c
@@ -33,6 +33,7 @@ int main() {
 		"Hello,... World.",
 		"69test 420numbers.",
 		"69 test 420 numbers.",
+		"memes \n dank.",
 		"same tbh. lmao",
 		"AYY LMAO. ME tOO thanks.",
 		"The Heisenbug is an incredible creature. Facenovel servers get their power from its indeterminism. Code smell can be ignored with INCREDIBLE use of air freshener. God objects are the new religion.",
