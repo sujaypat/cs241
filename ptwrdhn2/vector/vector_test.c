4709a34302b11ed8710f4ec365adc06a87638f1d
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-08 03:19:39 +0000

    o

--- ptwrdhn2/vector/vector_test.c
+++ ptwrdhn2/vector/vector_test.c
@@ -1,9 +1,15 @@
 /**
- * Machine Problem: Vector
- * CS 241 - Fall 2016
- */
+* Machine Problem: Vector
+* CS 241 - Fall 2016
+*/
 
 #include "vector.h"
 
 // Test your vector here
-int main() { return 0; }
+int main() {
+
+	
+
+
+	return 0;
+}
