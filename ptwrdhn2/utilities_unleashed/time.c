3274b561f6b60af0c059fb12e18af227d502775b
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-12 02:56:44 +0000

    util6

--- ptwrdhn2/utilities_unleashed/time.c
+++ ptwrdhn2/utilities_unleashed/time.c
@@ -8,12 +8,16 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
+	char *child_argv[] = NULL;
+	for(int i = 1; i < argc; i++){
+		child_argv[i - 1] = argv[i];
+	}
 	struct timespec start, end;
 	if(!strcmp(argv[0],"time"))
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	pid_t p = fork();
 	if(p == 0){
-		exec
+		execvp(child_argv[0], child_argv[]);
 	}
 	int status;
 	waitpid(p, status, WUNTRACED);
