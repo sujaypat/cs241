2387e5a1f7f4d753d0cb8b5b0244c689f6bb2edb
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-12 03:05:05 +0000

    util13

--- ptwrdhn2/utilities_unleashed/env.c
+++ ptwrdhn2/utilities_unleashed/env.c
@@ -2,8 +2,18 @@
 * Lab: Utilities Unleashed
 * CS 241 - Fall 2016
 */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+
+extern char **environ;
 
 int main(int argc, char *argv[]) {
-	
+	for (char **env = environ; *env; ++env)
+        printf("%s\n", *env);
 	return 0;
 }
