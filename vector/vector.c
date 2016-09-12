a1157c5e36293c5e31271bbf2a6a8a36cbc5e5b6
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-12 00:43:13 +0000

    vec13

--- vector/vector.c
+++ vector/vector.c
@@ -76,26 +76,29 @@ size_t Vector_capacity(Vector *vector) {
 void Vector_resize(Vector *vector, size_t new_size) {
 	assert(vector);
 	assert(new_size >= 0);
+	size_t initSize = Vector_size(vector);
 	size_t initCap = Vector_capacity(vector);
 	// your code here
-	if(new_size > initCap){
+	if(new_size > initCap && new_size > 10){
 		while(new_size > Vector_capacity(vector)){
 			vector -> capacity *= 2;
 		}
-		//memset vector array + initial capacity
-		vector -> array = realloc(vector -> array, Vector_capacity(vector) * sizeof(void *));
-		memset(vector -> array + initCap, 0, (initCap - Vector_capacity(vector) * sizeof(void *)));
+		vector -> array = realloc(vector -> array, new_size * sizeof(void *));
+		memset(vector -> array + initSize, 0, (initSize - new_size) * sizeof(void *)));
 	}
-	else if(4 * new_size < Vector_capacity(vector)){
-		while (Vector_capacity(vector) > 2 * new_size) {
-			vector -> capacity /= 2;
+	else if(4 * new_size <= Vector_capacity(vector) || new_size <= 10){
+		if(new_size > 10){
+			while (Vector_capacity(vector) > 2 * new_size) {
+				vector -> capacity /= 2;
+			}
 		}
-		for(size_t i = Vector_capacity(vector); i < initCap; i++){
+		else vector -> capacity = 10;
+		for(size_t i = new_size; i <= initSize; i++){
 			if(vector -> array[i]){
 				vector -> destructor(vector -> array[i]);
 			}
 		}
-		vector -> array = realloc(vector -> array, Vector_capacity(vector) * sizeof(void *));
+		vector -> array = realloc(vector -> array, new_size * sizeof(void *));
 	}
 	if(Vector_size(vector) > Vector_capacity(vector)) vector -> size = Vector_capacity(vector);
 }
@@ -103,6 +106,8 @@ void Vector_resize(Vector *vector, size_t new_size) {
 void Vector_set(Vector *vector, size_t index, void *elem) {
 	assert(vector);
 	assert(index >= 0);
+	assert(index < Vector_size(vector));
+	assert(Vector_size(vector));
 	// your code here
 	if(index < Vector_capacity(vector)){
 		if(vector -> array[index]) vector -> destructor(vector -> array[index]);
