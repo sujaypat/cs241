0f649a6e9bc0ff534547c4e456d3ef061055e560
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-12 04:22:27 +0000

    util21

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -13,9 +13,6 @@
 extern char **environ;
 
 int main(int argc, char *argv[]) {
-	char *vars = argv[1];
-	char *func = argv[2];
-	char *arg = argv[3];
 	for (char **env = environ; *env; ++env)
         printf("%s\n", *env);
 	return 0;
