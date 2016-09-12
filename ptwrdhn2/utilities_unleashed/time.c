8e3f45c26f61184de6e7b45f4dfcaa03d37a2536
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-12 02:25:40 +0000

    util3

--- ptwrdhn2/utilities_unleashed/time.c
+++ ptwrdhn2/utilities_unleashed/time.c
@@ -1,7 +1,20 @@
 /**
- * Lab: Utilities Unleashed
- * CS 241 - Fall 2016
- */
+* Lab: Utilities Unleashed
+* CS 241 - Fall 2016
+*/
 
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+	if(argv[0] == "time")
+	time_t start = clock_gettime();
+	pid_t p = fork();
+	if(p == 0){
+		exec
+	}
+	int status;
+	waitpid(p, status, WUNTRACED);
+	// fork and get parent time
+	// then exec sleep and get time on child
+	// compare timestamps
+	return 0;
+}
