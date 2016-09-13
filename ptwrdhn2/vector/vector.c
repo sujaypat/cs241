6220e483ebe5dcb136248e6af67ebe16c3c37cef
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-13 18:33:57 +0000

    vec72

--- ptwrdhn2/vector/vector.c
+++ ptwrdhn2/vector/vector.c
@@ -142,9 +142,8 @@ void Vector_delete(Vector *vector, size_t index) {
 	// your code here, what asserts might you want?
 
 	if(vector -> array[index]) vector -> destructor(vector -> array[index]);
-
 	// memmove(vector -> array[index], vector -> array[index + 1], (Vector_size(vector) - index - 1) * sizeof(void *));
-	for(size_t i = index; i < (Vector_size(vector) - index - 1); i++){
+	for(size_t i = index; i < (Vector_size(vector) - 1); i++){
 		vector -> array[i] = vector -> array[i + 1];
 	}
 	Vector_resize(vector, Vector_size(vector) - 1);
