cef7e8403380132d7078a03420dda13de3ae7099
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-13 00:27:29 +0000

    vec35
    svn ci -m "vec35"
    svn ci -m "vec35"
    "
    
    svn ci -m "vec35"
    
    
    
    
    
    
    
    
    
    
    svn ci -m "vec35"
    svn ci -m "vec35"
    "

--- ptwrdhn2/vector/vector_test.c
+++ ptwrdhn2/vector/vector_test.c
@@ -50,7 +50,7 @@ int main() {
 	Vector_append(test, val);
 	Vector_append(test, val);
 	Vector_append(test, val);
-	dumplmaos(test);
+	// dumplmaos(test);
 	Vector_set(test, 20, val);
 	Vector_delete(test, Vector_size(test) - 1);
 	Vector_destroy(test);
