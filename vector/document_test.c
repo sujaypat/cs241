44d7423e1a3fe40bf7a81d82e53bfdee8d6c812e
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-13 23:25:02 +0000

    vec92

--- vector/document_test.c
+++ vector/document_test.c
@@ -1,7 +1,7 @@
 /**
- * Machine Problem: Vector
- * CS 241 - Fall 2016
- */
+* Machine Problem: Vector
+* CS 241 - Fall 2016
+*/
 
 #include "document.h"
 #include <assert.h>
@@ -20,8 +20,13 @@ int main() {
 	Document *d_file10 = Document_create_from_file("memes10.txt");
 	printf("line 1: %s\n", Document_get_line(d_file10, 1));
 	printf("line 8: %s\n", Document_get_line(d_file10, 8));
-	Document_set_line(d_file10, 4, "ldhlkadhvlkadjhvkdjfvsdfjbvjdsfkjsdfkjdshfjdshkhjsfb");
 	Document_insert_line(d_file10, 24, "line 24");
+	Document_set_line(d_file10, 4, "ldhlkadhvlkadjhvkdjfvsdfjbvjdsfkjsdfkjdshfjdshkhjsfb");
+	Document_set_line(d_file10, 11, "ldhlkadhvlkadjhvkdjfvsdfjbvjdsfkjsdf0890890890hfjdshkhjsfb");
+	Document_set_line(d_file10, 13, "ldhlkadhvlkadjhvkdjfvsdfjbvjdsfkj8678678jdshfjdshkhjsfb");
+	Document_set_line(d_file10, 17, "ldhlkadhvlkadjhvkdjfvsdfjbvjd34534534dfkjdshfjdshkhjsfb");
+	Document_set_line(d_file10, 19, "ldhlkadhvlkadjhvkdjfvsdfj435345345fkjsdfkjdshfjdshkhjsfb");
+	Document_set_line(d_file10, 25, "ldhlkadhvlkadjhvkdjfvsdfjb6345sfkjsdfkjdshfjdshkhjsfb");
 	Document_delete_line(d_file10, 7);
 	Document_write_to_file(d_file10, "same.txt");
 	Document_destroy(d_file10);
