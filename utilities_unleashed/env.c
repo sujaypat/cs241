9b34574b5569abe19a42b63e236accf1cd50d464
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-13 02:12:00 +0000

    util30

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -2,18 +2,46 @@
 * Lab: Utilities Unleashed
 * CS 241 - Fall 2016
 */
+#include "format.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 
 extern char **environ;
 
 int main(int argc, char *argv[]) {
-	for (char **env = environ; *env; ++env)
-        printf("%s\n", *env);
+	if(argc == 1){
+		for (char **env = environ; *env; ++env){
+			printf("%s\n", *env);
+		}
+	}
+	int status_env, status_cmd;
+	char *env_args = NULL;
+
+	// if parent, fork for env changes, fork for command after
+	// 1 arg for env, 2 args for command
+	else{
+		pid_t env_change = fork();
+		if(env_change == 0){
+			waitpid(env_change, &status_env, 0);
+			pid_t command = fork();
+			if(command == 0){
+				waitpid(command, &status_cmd, 0);
+			}
+			else if(command > 0){
+				execvp(argv[2], argv + 2);
+			}
+		}
+		else if(env_change > 0){
+			execvp(argv[1], argv);
+		}
+	}
+
+
 	return 0;
 }
--- utilities_unleashed/time.c
+++ utilities_unleashed/time.c
@@ -13,23 +13,20 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-	char *child_argv[argc];
 	int status;
-		struct timespec start, end;
+	struct timespec start, end;
 
 	if(argc < 3) print_time_usage();
 
-	for(int i = 1; i <= argc; i++){
-		child_argv[i - 1] = argv[i];
-	}
 
+	// fork exec wait block with timing
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	pid_t p = fork();
 	if(p > 0){
 		waitpid(p, &status, 0);
 	}
 	else if(p == 0){
-		execvp(child_argv[0], child_argv);
+		execvp(argv[1], argv + 1);
 		print_exec_failed();
 	}
 	else{
@@ -37,6 +34,7 @@ int main(int argc, char *argv[]) {
 	}
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
+	// calculate time difference
 	double time_diff = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(1000000000);
 	display_results(argv, time_diff);
 
