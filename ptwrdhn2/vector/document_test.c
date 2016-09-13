e7a795d67a0307da296a3647939b66d41340dba4
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-13 21:44:19 +0000

    vec78

--- ptwrdhn2/vector/document_test.c
+++ ptwrdhn2/vector/document_test.c
@@ -7,11 +7,16 @@
 // test your document here
 int main() {
 	Document *d = Document_create();
-
-
-
-
-	
 	Document_destroy(d);
+
+	Document *d_file = Document_create_from_file("memes1.txt");
+	printf("line 1: %s\n", Document_get_line(d_file, 1));
+	printf("line 2: %s\n", Document_get_line(d_file, 2));
+	Document_destroy(d_file);
+
+	Document *d_file = Document_create_from_file("memes10.txt");
+	printf("line 1: %s\n", Document_get_line(d_file, 1));
+	printf("line 8: %s\n", Document_get_line(d_file, 8));
+	Document_destroy(d_file);
 	return 0;
 }
