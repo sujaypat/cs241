0efa608624b5aa2364007f901ccb887b761a58ae
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-13 23:18:35 +0000

    vec89

--- ptwrdhn2/vector/vector.c
+++ ptwrdhn2/vector/vector.c
@@ -158,7 +158,7 @@ void Vector_delete(Vector *vector, size_t index) {
 		vector -> array[i] = vector -> array[i + 1];
 	}
 	vector -> array[(Vector_size(vector) - 1)] = NULL;
-	if(vector -> array[index]) vector -> destructor(vector -> array[(Vector_size(vector) - 1)]);
+	// if(vector -> array[index]) vector -> destructor(vector -> array[(Vector_size(vector) - 1)]);
 	vector -> array[(Vector_size(vector) - 1)] = NULL;
 }
 
