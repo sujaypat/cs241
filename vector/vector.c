28d7e08b18a311b777b423db2a45cc3485482238
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-13 23:55:28 +0000

    vec104

--- vector/vector.c
+++ vector/vector.c
@@ -154,10 +154,11 @@ void Vector_delete(Vector *vector, size_t index) {
 
 	if(vector -> array[index]) vector -> destructor(vector -> array[index]);
 	vector -> array[index] = NULL;
-	for(size_t i = index; i < (Vector_size(vector)); i++){
+	for(size_t i = index; i < (Vector_size(vector) - 1); i++){
 		vector -> array[i] = vector -> array[i + 1];
 	}
-	// vector -> array[(Vector_size(vector) - 1)] = NULL;
+	if(vector -> array[index]) vector -> destructor(vector -> array[(Vector_size(vector) - 1)]);
+	vector -> array[(Vector_size(vector) - 1)] = NULL;
 }
 
 void Vector_append(Vector *vector, void *elem) {
