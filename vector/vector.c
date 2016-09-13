ce6177d274c14077038da7b62c838cfba5a39ae5
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-13 21:06:02 +0000

    vec74

--- vector/vector.c
+++ vector/vector.c
@@ -142,8 +142,9 @@ void Vector_delete(Vector *vector, size_t index) {
 	// your code here, what asserts might you want?
 
 	if(vector -> array[index]) vector -> destructor(vector -> array[index]);
+	vector -> array[index] = NULL;
 	// memmove(vector -> array[index], vector -> array[index + 1], (Vector_size(vector) - index - 1) * sizeof(void *));
-	for(size_t i = index; i < (Vector_size(vector) - 2); i++){
+	for(size_t i = index; i < (Vector_size(vector) - 1); i++){
 		vector -> array[i] = vector -> array[i + 1];
 	}
 	Vector_resize(vector, Vector_size(vector) - 1);
