ba4c8a513ec7ffb9bf2bc8ac8fcec7b32f3cd61a
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-14 15:44:54 +0000

    editor1

--- ptwrdhn2/text_editor/editor.c
+++ ptwrdhn2/text_editor/editor.c
@@ -1,7 +1,7 @@
 /**
- * Machine Problem: Text Editor
- * CS 241 - Fall 2016
- */
+* Machine Problem: Text Editor
+* CS 241 - Fall 2016
+*/
 
 #include "document.h"
 #include "editor.h"
@@ -13,32 +13,46 @@
 #include <string.h>
 
 char *get_filename(int argc, char *argv[]) {
-  // TODO implement get_filename
-  return argv[1];
-  // take a look at editor_main.c to see what this is used for
-  return NULL;
+	// TODO implement get_filename
+	return argc == 2 ? argv[1] : NULL;
+	// take a look at editor_main.c to see what this is used for
+	// return NULL;
 }
 
 void handle_display_command(Document *document, const char *command) {
-  // TODO implement handle_display_command
+	// TODO implement handle_display_command
+	if(strlen(command) == 1){
+		//print full document if exists
+		for(size_t i = 0; i < Document_size(document); i++){
+			print_line(document, i);
+		}
+	}
+	else{
+		size_t line_num = stoi(command + 2);
+		for(size_t j = line_num - 5; j < line_num + 5; j++){
+			if(j >= 0 && j < Document_size(document)){
+				print_line(document, j);
+			}
+		}
+	}
 }
 
 void handle_write_command(Document *document, const char *command) {
-  // TODO implement handle_write_command
+	// TODO implement handle_write_command
 }
 
 void handle_append_command(Document *document, const char *command) {
-  // TODO implement handle_append_command
+	// TODO implement handle_append_command
 }
 
 void handle_delete_command(Document *document, const char *command) {
-  // TODO implement handle_delete_command
+	// TODO implement handle_delete_command
 }
 
 void handle_search_command(Document *document, const char *command) {
-  // TODO implement handle_search_command
+	// TODO implement handle_search_command
 }
 
 void handle_save_command(Document *document, const char *filename) {
-  // TODO implement handle_save_command
+	// TODO implement handle_save_command
 }
