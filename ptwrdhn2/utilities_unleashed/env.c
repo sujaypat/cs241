89a74863e7405e6e8ebb982d7f6e2047148c5c7d
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-15 02:09:17 +0000

    util55

--- ptwrdhn2/utilities_unleashed/env.c
+++ ptwrdhn2/utilities_unleashed/env.c
@@ -22,7 +22,7 @@ char * replace_vars(char * input){
 	char *end = NULL;
 	beg = strsep(&input, "%%");
 	while(*input){
-		if(!(isalpha(*input) || isdigit(*input) || !strcmp(*input,"_"))){
+		if(!(isalpha(*input) || isdigit(*input)) || strcmp('_', *input)){
 			break;
 		}
 		var = strcat(var, *input);
