eed9b923d1888e214b7e30420b80e5e76e6ac4e1
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-16 18:01:57 +0000

    valgrind12

--- mini_valgrind/mini_valgrind.c
+++ mini_valgrind/mini_valgrind.c
@@ -141,7 +141,7 @@ void remove_meta_data(void *ptr) {
 	meta_data *curr = head;
 	while(curr != NULL){
 		if(curr -> next == ptr && ptr){
-			curr -> next = (*ptr) -> next;
+			curr -> next = (*ptr).next;
 			free(ptr);
 			total_free += curr -> size;
 			break;
@@ -164,7 +164,7 @@ void destroy() {
 		meta_data *p1 = head;
 		meta_data *p2 = head;
 		while(p1 != NULL && p2 != NULL){
-			p2 = (*p1) -> next;
+			p2 = (*p1).next;
 			free(p1);
 			p1 = p2;
 		}
