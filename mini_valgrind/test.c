283f783303aa47aeca988ea9f4c536f0bcf09d43
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-16 17:37:11 +0000

    valgrind5

--- mini_valgrind/test.c
+++ mini_valgrind/test.c
@@ -1,15 +1,27 @@
 /**
- * Mini Valgrind Lab
- * CS 241 - Fall 2016
- */
+* Mini Valgrind Lab
+* CS 241 - Fall 2016
+*/
 
 #include "mini_valgrind.h"
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-  // your tests here using malloc and free
+	// your tests here using malloc and free
+	char * f = "test.c";
+	void * p1 = mini_malloc(32, f, 12);
+	printf("p1 => %p\n", p1);
 
-  // Do NOT modify this line
-  atexit(print_report);
-  return 0;
+	void * p2 = mini_malloc(64, f, 15);
+	printf("p2 => %p\n", p2);
+
+	mini_free(p1);
+
+	mini_free(p2);
+
+	mini_free(p1 - 50);
+
+	// Do NOT modify this line
+	atexit(print_report);
+	return 0;
 }
