832d18b67494cfbac9f3ea9d3b00426c8b06d93f
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-16 04:46:44 +0000

    editor58

--- ptwrdhn2/text_editor/editor.c
+++ ptwrdhn2/text_editor/editor.c
@@ -40,7 +40,8 @@ void handle_write_command(Document *document, const char *command) {
 	char *start = (char *)(command + 2);
 	printf("%p\n", &start);
 	printf("%s\n", start);
-	char *num = strsep(&start, " ");
+	char *num = malloc(strlen(start));
+	num = strsep(&start, " ");
 	int line_num = atoi(num);
 	char *end = start;
 	char *res;
