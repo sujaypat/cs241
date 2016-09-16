f26faeb2c426c0c0dcef268c2ea3dbc9b620f575
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-16 22:49:26 +0000

    valgrind40

--- ptwrdhn2/mini_valgrind/test.c
+++ ptwrdhn2/mini_valgrind/test.c
@@ -8,19 +8,7 @@
 #include <stdlib.h>
 int main() {
 	// your tests here using malloc and free
-	void * p1 = mini_malloc(32, __FILE__, __LINE__);
-	printf("p1 => %p\n", p1);
 
-	void * p2 = mini_malloc(64, __FILE__, __LINE__);
-	printf("p2 => %p\n", p2);
-
-	mini_free(p1);
-
-	mini_free(p2);
-
-	mini_free(p1 - 50);
-
-	destroy();
 	// Do NOT modify this line
 	atexit(print_report);
 	return 0;
