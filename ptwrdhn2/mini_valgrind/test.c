0f24afa8a48a68436f6fa6d954db7b83bf055d29
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-16 20:44:07 +0000

    valgrind27

--- ptwrdhn2/mini_valgrind/test.c
+++ ptwrdhn2/mini_valgrind/test.c
@@ -16,11 +16,11 @@ int main() {
 
 	mini_free(p1);
 
-	// mini_free(p2);
+	mini_free(p2);
 
 	mini_free(p1 - 50);
 
-	destroy();
+	// destroy();
 	// Do NOT modify this line
 	atexit(print_report);
 	return 0;
