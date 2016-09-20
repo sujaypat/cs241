5185057ba0ef709bee0dc3eab2e8c6aae45bcfc5
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-20 01:44:29 +0000

    shell16

--- shell/shell.c
+++ shell/shell.c
@@ -32,13 +32,61 @@ int shell(int argc, char *argv[]) {
 		else if(!strcmp(argv[argc - 2], "-f")) handle_file(argv[argc - 1]);
 		else print_usage();
 	}
-
 	print_shell_owner("ptwrdhn2");
 	signal(SIGINT, sigint_handler);
-	while(1){
-		print_prompt(getenv("PWD"), getpid());
 
-		if(EOF) break;
+
+	char *command = NULL;
+	size_t len = 0;
+	int done = 0;
+
+	while (!done) {
+		print_prompt(getenv("PWD"), getpid());
+		getline(&command, &len, stdin);
+
+		// remove newline from the command
+		char *nl = strchr(command, '\n');
+		if (nl)
+		*nl = 0;
+
+
+		int command_type = command[0];
+		switch (command_type) {
+			case 'p':
+			handle_display_command(document, command);
+			break;
+			case 'w':
+			handle_write_command(document, command);
+			break;
+			case 'a':
+			handle_append_command(document, command);
+			break;
+			case 'd':
+			handle_delete_command(document, command);
+			break;
+			case '/':
+			handle_search_command(document, command);
+			break;
+			case 's':
+			if (strlen(command) == 1) {
+				handle_save_command(document, filename);
+			} else {
+				invalid_command(command);
+			}
+			break;
+			case 'q':
+			done = 1;
+			Document_destroy(document);
+			break;
+			default:
+			fprintf(stderr, "Something bad happened! Report to CS241 Staff!\n");
+			break;
+		}
+	}
+
+	// Need to free the buffer that we created.
+	if (command) {
+		free(command);
 	}
 
 	return 0;
