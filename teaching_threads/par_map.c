d62f9928fc9dbc710608b06ef60c2880c06ee127
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-22 20:16:01 +0000

    teach1

--- teaching_threads/par_map.c
+++ teaching_threads/par_map.c
@@ -1,23 +1,35 @@
 /**
- * Parallel Map Lab
- * CS 241 - Fall 2016
- */
+* Parallel Map Lab
+* CS 241 - Fall 2016
+*/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include "map.h"
 #include "mappers.h"
 
 /* You should create a struct that will get passed in by reference to your
- * start_routine. */
-
+* start_routine. */
+typedef struct args{
+	double d;
+} args;
 /* You should create a start routine for your threads. */
 
-double *par_map(double *list, size_t list_len, mapper map_func,
-                size_t num_threads) {
-  /* Your implementation goes here */
-  return NULL;
+double *par_map(double *list, size_t list_len, mapper map_func, size_t num_threads) {
+	double *res = (double *)malloc(list_len * sizeof(double));
+
+
+	pthread_t threads[num_threads];
+	for(int i = 0; i < min(list_len, num_threads); i++){
+		args -> d = list[i];
+		pthread_create(threads + i, NULL, map_func, args);
+	}
+	for(int i = 0; i < min(list_len, num_threads); i++){
+		pthread_join(threads + i, NULL);
+	}
+	return res;
 }
