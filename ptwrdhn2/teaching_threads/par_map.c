cc94d654f5f896a1f9fc5fc39204a32db19ee396
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-22 20:31:04 +0000

    teach6

--- ptwrdhn2/teaching_threads/par_map.c
+++ ptwrdhn2/teaching_threads/par_map.c
@@ -16,21 +16,32 @@
 * start_routine. */
 typedef struct args{
 	double d;
+	mapper func;
 } args;
 /* You should create a start routine for your threads. */
+
+
+void *routine(void *ptr){
+	map_func(ptr.d);
+}
 
 double *par_map(double *list, size_t list_len, mapper map_func, size_t num_threads) {
 	double *res = (double *)malloc(list_len * sizeof(double));
 
 	args a;
+	a.func = map_func;
 	int min = list_len < num_threads ? list_len : num_threads;
-	pthread_t threads[num_threads];
-	for(int i = 0; i < min; i++){
-		a.d = list[i];
-		pthread_create(threads + i, NULL, *map_func, &a);
-	}
-	for(int i = 0; i < min; i++){
-		pthread_join(*(threads + i), NULL);
+	pthread_t threads[min];
+	for(int same = 0; same < list_len; same+= num_threads){
+
+		for(int i = 0; i < num_threads; i++){
+			a.d = list[i];
+			pthread_create(threads + i, NULL, map_func, &a);
+		}
+
+		for(int i = 0; i < min; i++){
+			pthread_join(*(threads + i), NULL);
+		}
 	}
 	return res;
 }
