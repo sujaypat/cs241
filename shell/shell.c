570716daa5643745e3107bf3839fcdb9f77f36ff
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-09-23 19:39:14 +0000

    shell104

--- shell/shell.c
+++ shell/shell.c
@@ -171,13 +171,13 @@ int shell(int argc, char *argv[]) {
 		if(!script) eof = getline(&command, &len, stdin);
 		else eof = getline(&command, &len, f);
 		if(eof == -1) break;
-		if(len == 1) continue;
+		// if(len == 1) continue;
 
 		// remove newline from the command
 		char *nl = strchr(command, '\n');
 		if (nl) *nl = 0;
 		if(script) print_command(command);
-		if(command){
+		if(command && len){
 			if(!strncmp(command, "cd", 2)) handle_cd(command);
 			else if(!strncmp(command, "!history", 8)) handle_history();
 			else if(!strncmp(command, "#", 1)) handle_num_history(command);
