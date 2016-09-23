0269b0f8bc74124bc6c32c8a513b661c4e377ca3
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-23 23:37:57 +0000

    teach55

--- ptwrdhn2/teaching_threads/par_map.c
+++ ptwrdhn2/teaching_threads/par_map.c
@@ -53,7 +53,7 @@ double *par_map(double *list, size_t list_len, mapper map_func, size_t num_threa
 		arguments[index] -> func = map_func;
 		arguments[index] -> start_index = index * block_size;
 		arguments[index] -> end_index = ((index + 1) * block_size - 1);
-		fprintf(stderr, "%zu", index * block_size);
+		fprintf(stderr, "PROD IS %zu\n", index * block_size);
 
 		arguments[index] -> list = list;
 		arguments[index] -> res = res;
