d027acb946b682c25311a40657ac6284ef25cf79
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-24 00:05:42 +0000

    teach58

--- ptwrdhn2/teaching_threads/par_map.c
+++ ptwrdhn2/teaching_threads/par_map.c
@@ -57,8 +57,9 @@ double *par_map(double *list, size_t list_len, mapper map_func, size_t num_threa
 
 	}
 	if(((index + 1) * block_size - 1) != list_len - 1){
+		index++;
 		(arguments+index) -> func = map_func;
-		(arguments+index) -> start_index = (index + 1) * block_size;
+		(arguments+index) -> start_index = (index) * block_size;
 		(arguments+index) -> end_index = list_len - 1;
 
 		(arguments+index) -> list = list;
