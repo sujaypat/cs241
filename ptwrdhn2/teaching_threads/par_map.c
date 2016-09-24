7d23bf2420a8aa7cdd912aeb723add2de94ce2bb
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-24 00:28:35 +0000

    teach60

--- ptwrdhn2/teaching_threads/par_map.c
+++ ptwrdhn2/teaching_threads/par_map.c
@@ -57,7 +57,10 @@ double *par_map(double *list, size_t list_len, mapper map_func, size_t num_threa
 
 	}
 	// if(((index + 1) * block_size - 1) != list_len - 1){
+
 	index = num_threads-1;
+	threads[index] = malloc(sizeof(pthread_t));
+
 	(arguments+index) -> func = map_func;
 	(arguments+index) -> start_index = index * block_size;
 	(arguments+index) -> end_index = list_len - 1;
