b75babcac6cdd1f0cb98f2b2a53daf6fd33baec7
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-09-26 15:58:33 +0000

    shell130

--- ptwrdhn2/shell/shell.c
+++ ptwrdhn2/shell/shell.c
@@ -181,7 +181,6 @@ int shell(int argc, char *argv[]) {
 		if(nl) *nl = 0;
 
 		if(script) print_command(command);
-		printf("%s : %zu\n", command, len);
 		if(command && len != 120){
 			if(!strncmp(command, "cd", 2)) handle_cd(command);
 			else if(!strncmp(command, "!history", 8)) handle_history();
