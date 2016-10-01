a780a51163fe9cdba8a5351fc1ca94176eae5cce
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-10-01 22:53:42 +0000

    malloc52

--- malloc/alloc.c
+++ malloc/alloc.c
@@ -38,7 +38,7 @@ void *first_fit(size_t size_needed){
 	meta_data *curr = head;
 	while(curr != NULL){
 		write(0, &(curr -> size), sizeof(size_t));
-		write(0, " = size", strlen(" = size") + 1);
+		write(0, " = size\n", strlen(" = size\n") + 1);
 		if(curr -> size >= size_needed && curr -> is_free){
 			found = curr;
 			break;
