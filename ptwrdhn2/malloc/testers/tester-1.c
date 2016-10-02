9cadb12a2eb027b34061b0395cf26318af578442
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-02 03:55:22 +0000

    malloc93

--- ptwrdhn2/malloc/testers/tester-1.c
+++ ptwrdhn2/malloc/testers/tester-1.c
@@ -1,7 +1,7 @@
 /**
- * Machine Problem: Malloc
- * CS 241 - Fall 2016
- */
+* Machine Problem: Malloc
+* CS 241 - Fall 2016
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,19 +9,21 @@
 #define NUM_CYCLES 10000000
 
 int main() {
-  int i;
-  for (i = 0; i < NUM_CYCLES; i++) {
-    int *ptr = malloc(sizeof(int));
+	int i;
+	for (i = 0; i < NUM_CYCLES; i++) {
+		int *ptr = malloc(sizeof(int));
 
-    if (ptr == NULL) {
-      printf("Memory failed to allocate!\n");
-      return 1;
-    }
-	ptrintf("%p\n", sbrk(0));
-    *ptr = 4;
-    free(ptr);
-  }
+		if (ptr == NULL) {
+			printf("Memory failed to allocate!\n");
+			return 1;
+		}
+		printf("sbrk %p\n", sbrk(0));
+		*ptr = 4;
+		printf("value %d\n", *ptr);
+		printf("loc %p\n", ptr);
+		free(ptr);
+	}
 
-  printf("Memory was allocated, used, and freed!\n");
-  return 0;
+	printf("Memory was allocated, used, and freed!\n");
+	return 0;
 }
