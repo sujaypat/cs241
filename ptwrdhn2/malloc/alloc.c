e76387b007381d3b9255649bcc64d84112a3557e
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-03 04:22:35 +0000

    malloc122

--- ptwrdhn2/malloc/alloc.c
+++ ptwrdhn2/malloc/alloc.c
@@ -214,6 +214,11 @@ void free(void *in) {
 void *realloc(void *ptr, size_t size) {
 	meta_data *curr = (meta_data *)(ptr - sizeof(meta_data));
 	if(size == curr -> size) return ptr;
+	if(size == 0){
+		free(ptr);
+		return NULL;
+	}
+	if(ptr == NULL) return malloc(size);
 	// if(size > curr -> size){
 	// 	if(curr -> next -> is_free && size < ((curr -> size) + sizeof(meta_data) + (curr -> next -> size))){
 	// 		curr -> size = size;
