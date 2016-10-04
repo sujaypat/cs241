c41c32b04ddbf0885b66008b090dd24b7eb5d44c
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-04 01:49:48 +0000

    locks6

--- ptwrdhn2/luscious_locks/queue_tests.c
+++ ptwrdhn2/luscious_locks/queue_tests.c
@@ -10,8 +10,35 @@
 
 #include "queue.h"
 
-int main(int argc, char **argv) {
-
-	printf("Please write tests cases\n");
-	return 0;
+void *sender(void * queue) {
+    send(queue, "hello, ");
+    send(queue, "world");
+    send(queue, "how are you?");
+    printf("sent data\n");
 }
+
+void *recver(void * queue) {
+    sleep(1);
+    printf("received '%s'\n", recv(queue));
+    printf("received '%s'\n", recv(queue));
+    printf("received '%s'\n", recv(queue));
+}
+
+int main() {
+    mq_t * queue = create();
+
+    pthread_t s, r;
+    pthread_create(&s, null, sender, (void*)queue);
+    pthread_create(&r, null, recver, (void*)queue);
+    pthread_join(s, null);
+    pthread_join(r, null);
+
+    return 0;
+}
+
+
+// int main(int argc, char **argv) {
+//
+// 	printf("Please write tests cases\n");
+// 	return 0;
+// }
