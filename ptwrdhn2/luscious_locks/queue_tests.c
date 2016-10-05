93153d7d1a0f967053996b251dcd763997707501
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-05 23:32:57 +0000

    locks47

--- ptwrdhn2/luscious_locks/queue_tests.c
+++ ptwrdhn2/luscious_locks/queue_tests.c
@@ -24,7 +24,7 @@ void* pusher(void *p)
 {
 	int i = (*(int*)p);
 	int j =0 ;
-	for(j = 0; j < 2*i ;j++)
+	for(j = 0; j < i ;j++)
 	{
 		int* i = malloc(sizeof(int));
 		*i = j;
@@ -57,8 +57,8 @@ void* puller(void* p)
 }
 int main(int argc, char **argv) {
 	pthread_t thread1, thread2,thread3;
-	int i = 500;
-	int j = 2* i ;
+	int i = 5000;
+	int j = i ;
 	queue = queue_create(-2);
 	pthread_create(&thread1,NULL,pusher,(void*)(&i));
 	pthread_create(&thread2,NULL,pusher,(void*)(&i));
