97f6c9f925acd33dab1c73dbeb807193cec0faf0
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-10-05 05:31:59 +0000

    locks31

--- luscious_locks/barrier.c
+++ luscious_locks/barrier.c
@@ -7,13 +7,16 @@
 // The returns are just for errors if you want to check for them.
 int barrier_destroy(barrier_t *barrier) {
 	int error = 0;
-	pthread_mutex_destroy(&(barrier -> mtx));
-	pthread_cond_destroy(&(barrier -> cv));
+	error += pthread_mutex_destroy(&(barrier -> mtx));
+	error += pthread_cond_destroy(&(barrier -> cv));
 	return error;
 }
 
 int barrier_init(barrier_t *barrier, unsigned int num_threads) {
 	int error = 0;
+	barrier -> n_threads = num_threads;
+	barrier -> count = 0;
+	barrier -> times_used = 0;
 	pthread_mutex_init(&(barrier -> mtx), NULL);
 	pthread_cond_init(&(barrier -> cv), NULL);
 
@@ -22,12 +25,12 @@ int barrier_init(barrier_t *barrier, unsigned int num_threads) {
 
 int barrier_wait(barrier_t *barrier) {
 	pthread_mutex_lock(&(barrier -> mtx));
+	barrier -> times_used++;
 	barrier -> count++;
-	if(barrier -> count == barrier -> n_threads) pthread_cond_broadcast(&(barrier -> cv));
-	else while(barrier -> count < barrier -> n_threads) pthread_cond_wait(&(barrier -> cv), &(barrier -> mtx));
+	if(barrier -> count == barrier -> n_threads) pthread_cond_broadcast(&barrier -> cv);
+	else while(barrier -> count < barrier -> n_threads) pthread_cond_wait(&barrier -> cv, &barrier -> mtx);
+	barrier -> count = 0;
+	pthread_cond_broadcast(&barrier -> cv);
 	pthread_mutex_unlock(&(barrier -> mtx));
-
-	
-
 	return 0;
 }
