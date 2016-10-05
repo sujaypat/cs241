e1844de4fabff9d67692a61ff7b7dfae818242fb
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-10-05 04:29:45 +0000

    locks21

--- luscious_locks/queue_tests.c
+++ luscious_locks/queue_tests.c
@@ -7,49 +7,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include "queue.h"
+typedef struct queue_node_t {
+	struct queue_node_t *next;
+	void *data;
+} queue_node_t;
+struct queue_t {
+	queue_node_t *head, *tail;
+	int size;
+	int maxSize;
+	pthread_cond_t cv;
+	pthread_mutex_t m;
+};
+queue_t* queue;
+void* pusher(void *p)
+{
+	int i = (*(int*)p);
+	int j =0 ;
+	for(j = 0; j < i ;j++)
+	{
+		int* i = malloc(sizeof(int));
+		*i = j;
 
-void *sender(void *queue) {
-    queue_push(queue, "hello, ");
-    queue_push(queue, "world");
-    queue_push(queue, "how are you?");
-	queue_push(queue, "hegfdllo, ");
-    queue_push(queue, "worlfsdgd");
-    queue_push(queue, "how arefdsg you?");
-	queue_push(queue, "hello345, ");
-    queue_push(queue, "world5234");
-    queue_push(queue, "how are 4533245you?");
-    printf("sent data\n");
-	return 0;
+		queue_push(queue,(void*)(i));
+	}
+	printf("queue size is now %d\n",queue->size);
+	return NULL;
 }
-
-void *recver(void *queue) {
-    sleep(1);
-    printf("received '%s'\n", queue_pull(queue));
-    printf("received '%s'\n", queue_pull(queue));
-    printf("received '%s'\n", queue_pull(queue));
-	sleep(6);
-	printf("received '%s'\n", queue_pull(queue));
-	printf("received '%s'\n", queue_pull(queue));
-	printf("received '%s'\n", queue_pull(queue));
-	sleep(4);
-	printf("received '%s'\n", queue_pull(queue));
-	printf("received '%s'\n", queue_pull(queue));
-	printf("received '%s'\n", queue_pull(queue));
-
-	return 0;
+void* puller(void* p)
+{
+	int i = (*(int*)p);
+	int n = (i/2) - 1;
+	int j =0 ;
+	int sum = 0;
+	int predicted = 0;
+	for(j = 0; j < i ;j++)
+	{
+		//sum += *((int*)queue_pull(queue));
+		sum += *((int*)queue_pull(queue));
+		predicted += j;
+	}
+	printf("queue size is now %d\n",queue->size);
+	printf("The sum is %d and it should be %d\n",sum, (n*n + n) );
+	return NULL;
 }
-
-int main() {
-    queue_t *queue = queue_create(2);
-
-    pthread_t s, r;
-    pthread_create(&s, NULL, sender, (void*)queue);
-    pthread_create(&r, NULL, recver, (void*)queue);
-    pthread_join(s, NULL);
-    pthread_join(r, NULL);
-
-	queue_destroy(queue);
-    return 0;
+int main(int argc, char **argv) {
+	pthread_t thread1, thread2,thread3;
+	int i = 5000000;
+	int j = 2* i ;
+	queue = queue_create(10000000);
+	pthread_create(&thread1,NULL,pusher,(void*)(&i));
+	pthread_create(&thread2,NULL,pusher,(void*)(&i));
+	pthread_join(thread1,NULL);
+	pthread_join(thread2,NULL);
+	pthread_create(&thread3,NULL,puller,(void*)(&j));
+	pthread_join(thread3,NULL);
+	return 0;
 }
