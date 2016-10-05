9de9c911662feb75d2e6e9ec96301465f9cfaeb6
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-05 23:29:20 +0000

    locks43

--- ptwrdhn2/luscious_locks/queue.c
+++ ptwrdhn2/luscious_locks/queue.c
@@ -75,7 +75,7 @@ void *queue_pull(queue_t *queue) {
 *  Returns a pointer to this allocated space.
 */
 queue_t *queue_create(int maxSize) {
-	queue_t *same = malloc(sizeof(queue_t) + maxSize*sizeof(queue_node_t));
+	queue_t *same = malloc(sizeof(queue_t));
 	same -> head = NULL;
 	same -> tail = NULL;
 	same -> size = 0;
