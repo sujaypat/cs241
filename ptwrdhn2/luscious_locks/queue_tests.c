9e59c61d9cbd9d6d00def2bd451495a1ff288b5f
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-05 20:16:28 +0000

    locks35

--- ptwrdhn2/luscious_locks/queue_tests.c
+++ ptwrdhn2/luscious_locks/queue_tests.c
@@ -61,11 +61,11 @@ int main(int argc, char **argv) {
 	int j = 2* i ;
 	queue = queue_create(1000000);
 	pthread_create(&thread1,NULL,pusher,(void*)(&i));
+	pthread_create(&thread3,NULL,puller,(void*)(&j));
+	pthread_join(thread3,NULL);
 	pthread_create(&thread2,NULL,pusher,(void*)(&i));
 	pthread_join(thread1,NULL);
 	pthread_join(thread2,NULL);
-	pthread_create(&thread3,NULL,puller,(void*)(&j));
-	pthread_join(thread3,NULL);
 	queue_destroy(queue);
 	return 0;
 }
