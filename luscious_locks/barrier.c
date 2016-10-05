1a3f8307479ebd20ff5449937cabcd565e43f2d5
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-10-05 02:56:10 +0000

    locks14

--- luscious_locks/barrier.c
+++ luscious_locks/barrier.c
@@ -23,9 +23,9 @@ int barrier_init(barrier_t *barrier, unsigned int num_threads) {
 int barrier_wait(barrier_t *barrier) {
 	pthread_mutex_lock(&(barrier -> mtx));
 	barrier -> count++;
-	if(count == barrier -> n_threads) pthread_cond_broadcast(&(barrier -> cv));
+	if(barrier -> count == barrier -> n_threads) pthread_cond_broadcast(&(barrier -> cv));
 	else{
-		while(count < barrier -> n_threads) pthread_cond_wait(&(barrier -> cv), &(barrier -> mtx));
+		while(barrier -> count < barrier -> n_threads) pthread_cond_wait(&(barrier -> cv), &(barrier -> mtx));
 	}
 	pthread_mutex_unlock(&(barrier -> mtx));
 	return 0;
