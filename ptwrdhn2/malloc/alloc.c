14b8083c24126a3d17f7ffd199aa07c28ebb7f9a
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-10 05:02:06 +0000

    idk anymore

--- ptwrdhn2/malloc/alloc.c
+++ ptwrdhn2/malloc/alloc.c
@@ -19,19 +19,19 @@ meta_data *tail = NULL;
 
 
 
-// void coalesce(void *same){
-// 	meta_data *a = same + sizeof(meta_data) + ((meta_data *)same) -> size;
-// 	meta_data *co = (meta_data *)same;
-// 	// if(a && a->size) write(0, "exists\n", 8);
-// 	if(((void *)a < ((void *)head + sizeof(meta_data) + head -> size)) && a -> free){
-// 		co -> size += a -> size + sizeof(meta_data);
-// 		if(a == head){
-// 			head = co;
-// 		}
-// 		if(((meta_data *)(same + sizeof(meta_data) + co -> size)) -> size){
-// 			((meta_data *)(same + sizeof(meta_data) + co -> size)) -> next = co;
-// 		}
-// 	}
+void coalesce(void *same){
+	meta_data *a = same + sizeof(meta_data) + ((meta_data *)same) -> size;
+	meta_data *co = (meta_data *)same;
+	// if(a && a->size) write(0, "exists\n", 8);
+	if(((void *)a < ((void *)head + sizeof(meta_data) + head -> size)) && a -> free){
+		co -> size += a -> size + sizeof(meta_data);
+		if(a == head){
+			head = co;
+		}
+		if(((meta_data *)(same + sizeof(meta_data) + co -> size)) -> size){
+			((meta_data *)(same + sizeof(meta_data) + co -> size)) -> next = co;
+		}
+	}
 	// if(co -> next && co -> next -> free){
 	// 	a = co -> next;
 	// 	a -> size += sizeof(meta_data) + co -> size;
@@ -39,7 +39,7 @@ meta_data *tail = NULL;
 	// 		((meta_data *)(co + sizeof(meta_data) + co -> size)) -> next = a;
 	// 	}
 	// }
-// }
+}
 
 
 /**
@@ -94,41 +94,54 @@ void *calloc(size_t num, size_t size) {
 * @see http://www.cplusplus.com/reference/clibrary/cstdlib/malloc/
 */
 void *malloc(size_t size) {
-
-	if(size == 0) return NULL;
-	meta_data *temp = head;
-
-	if(head != NULL){
-		if(head->size >= size){
-			head = head->next;
-			return (void*)temp+sizeof(meta_data);
-		}
-		else{
-			while(temp->next != NULL){
-				if(temp->next->size >= size){
-					meta_data *touse = temp->next;
-					temp->next = touse->next;
-
-					return (void*)touse+sizeof(meta_data);
-				}
-				temp = temp->next;
+	
+	meta_data *p = head;
+	meta_data *chosen = NULL;
+	if (size <= 0) return NULL;
+	if (is_free){
+		while (p != NULL) { // block splitting needs to be done here
+			if (p -> free && p -> size >= size){//} && p -> size <= size + sizeof(meta_data)) {
+				chosen = p;
+				break;
 			}
+			// else if(p -> free && p -> size > size + 2 * sizeof(meta_data)){
+			//
+			// 	meta_data *newBlock = (meta_data *)(((void*)p) + sizeof(meta_data) + size);
+			// 	newBlock -> next = p;
+			// 	newBlock -> free = 1;
+			// 	newBlock -> size = p -> size - sizeof(meta_data) - size;
+			// 	newBlock -> ptr = (((void *)newBlock) + sizeof(meta_data));
+			// 	if(p == head){
+			// 		head = newBlock;
+			// 	}
+			// 	else{
+			// 		((meta_data *)(((void*)p) + sizeof(meta_data) + p -> size)) -> next = newBlock;
+			// 	}
+			//
+			// 	// if(((void*)p) + sizeof(meta_data) + p -> size <= ((void*)head) + sizeof(meta_data) + head -> size){
+			// 	// 	((meta_data *)(((void*)p) + sizeof(meta_data) + p -> size)) -> next = newBlock;
+			// 	// }
+			// 	p -> size = size;
+			// 	chosen = p;
+			// 	break;
+			// }
+			p = p -> next;
+		}
+
+		if (chosen) {
+			chosen -> free = 0;
+			return chosen -> ptr;
 		}
 	}
-
-
-	void *p = sbrk(0);
-	p = sbrk(size+sizeof(meta_data));
-
-	if(p == (void*)-1) return NULL;
-
-	tail = p;
-	meta_data *cell = p;
-
-	cell->size = size;
-	cell->next = NULL;
-
-	return (void*)cell+sizeof(meta_data);
+	chosen = sbrk(0);
+	sbrk(sizeof(meta_data));
+	chosen -> ptr = sbrk(0);
+	if (sbrk(size) == (void*)-1) return NULL;
+	chosen -> size = size;
+	chosen -> free = 0;
+	chosen -> next = head;
+	head = chosen;
+	return chosen -> ptr;
 }
 
 /**
@@ -148,19 +161,11 @@ void *malloc(size_t size) {
 *    passed as argument, no action occurs.
 */
 void free(void *ptr) {
-	/*
 	if (!ptr) return;
 	meta_data *ptr2 = (meta_data*)ptr - 1;
 	ptr2 -> free = 1;
 	is_free = 1;
 	coalesce(ptr2);
-	return;
-	*/
-	if (!ptr) return;
-
-	meta_data *tofree = ptr-sizeof(meta_data);
-	tofree->next = head;
-	head = tofree;
 	return;
 }
 
@@ -211,37 +216,15 @@ void free(void *ptr) {
 */
 void *realloc(void *ptr, size_t size) {
 	if (!ptr) return malloc(size);
-	if (!size){
-		free(ptr);
-		return NULL;
-	}
-
-	meta_data *p = ptr-sizeof(meta_data);
-
-	if(p->size >= size) return ptr;
-
-	if(p->size < size) {
-		free(ptr);
-
-		void* newp = malloc(size);
-		memcpy(newp, ptr, p->size);
-
-		return newp;
-	}
-	return ptr;
-
-	/*
-	if (!ptr) return malloc(size);
 	if (size == 0) free(ptr);
 	meta_data *p = (meta_data*) ptr - 1;
 	if (p -> size >= size) {
-	return ptr;
-}
-else{
-void* out = malloc(size);
-memcpy(out, ptr, p -> size);
-free(ptr);
-return out;
-}
-*/
+		return ptr;
+	}
+	else{
+		void* out = malloc(size);
+		memcpy(out, ptr, p -> size);
+		free(ptr);
+		return out;
+	}
 }
