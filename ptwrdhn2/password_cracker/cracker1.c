89f2d11affd69e961dc007587716fcb2bf292b3e
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-12 00:23:14 +0000

    test

--- ptwrdhn2/password_cracker/cracker1.c
+++ ptwrdhn2/password_cracker/cracker1.c
@@ -1,13 +1,15 @@
 /**
- * Machine Problem: Password Cracker
- * CS 241 - Fall 2016
- */
+* Machine Problem: Password Cracker
+* CS 241 - Fall 2016
+*/
 
 #include "cracker1.h"
 #include "format.h"
 #include "utils.h"
 
 int start(size_t thread_count) {
-  // TODO your code here, make sure to use thread_count!
-  return 0;
+	
+
+
+	return 0;
 }
