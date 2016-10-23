2e9912073c472cb9a26118bb5e25b4fed3a4bd7f
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-10-23 02:23:46 +0000

    almost done

--- ptwrdhn2/password_cracker/cracker2.c
+++ ptwrdhn2/password_cracker/cracker2.c
@@ -16,125 +16,129 @@
 #include <string.h>
 #include <time.h>
 
-/*
-size_t numRecovered = 0, numFailed = 0, quit = 0, total = 0;
-pthread_mutex_t ct, tot;
+
+size_t quit = 0, total = 0;
+double cputime = 0;
+size_t tot_ct = 0;
+pthread_mutex_t q, tot;
+char *res;
 
 typedef struct thread_data{
-	queue_t *queue;
-	size_t	id;
+	char 	*start;
+	long	max;
+	size_t 	id;
+	char 	*target;
+	long 	offset;
+	char	*user;
 } td;
 
 void *start_routine(void *data){
 	td *info = (td *)data;
 	struct crypt_data cdata;
-	struct timespec start, end;
-	int incrementing = 1;
-
 	cdata.initialized = 0;
 
-	while(1){
+	size_t count = 0;
 
-		char *line = NULL;
-		line = queue_pull((info -> queue));
-		if(!line) break;
 
-		char *username = strsep(&line, " "); // username
-		char *result = strsep(&line, " "); // hashed password
-		char *incomplete = line; // part of password given
+	char *test = NULL;
+	int found = 0;
+	// printf("%s\n", info -> start);
+	char *first_dot = strchr(info -> start, '.');
+	setStringPosition(first_dot, info -> offset);
+	// printf("%s %zu\n", first_dot, info -> offset);
 
-		size_t count = 0;
-		v1_print_thread_start(info -> id, username);
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		char *test = NULL;
-		int found = 0;
-		size_t num_dots = 0;
-		char *where_is_dot = strchr(incomplete, '.');
-
-		for(size_t i = 0; i < strlen(incomplete); i++){
-			if(incomplete[i] == '.'){
-				incomplete[i] = 'a';
-				num_dots++;
-			}
+	v2_print_thread_start(info -> id, info -> user, (info -> offset), info -> start);
+	size_t i;
+	for(i = 0; i < (size_t)((info -> max)); i++){
+		// check if this thread should give up
+		pthread_mutex_lock(&q);
+		if(quit){
+			// printf("id: %zu gave up\n", info -> id);
+			pthread_mutex_unlock(&q);
+			break;
 		}
-		while(incrementing){
-			test = crypt_r(incomplete, "xx", &cdata);
-			count++;
-			if(!strcmp(result, test)){
-				found = 1;
-				break;
-			}
-			incrementing = incrementString(incomplete);
-			if(*where_is_dot){
-				if(*where_is_dot == 'z'){
-					where_is_dot++;
-				}
-			}
-			else incrementing = 0;
+		pthread_mutex_unlock(&q);
+		test = crypt_r(info -> start, "xx", &cdata);
+		count++;
+		if(!strcmp(info -> target, test)){
+			pthread_mutex_lock(&q);
+			quit = 1;
+			pthread_mutex_unlock(&q);
+			found = 1;
+			// printf("id: %zu found it\n", info -> id);
+			break;
 		}
-
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		double time_diff = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(1000000000);
-
-		if(incrementing && found){
-			v1_print_thread_result(info -> id, username, incomplete, count, time_diff, 0);
-			pthread_mutex_lock(&ct);
-			numRecovered++;
-			pthread_mutex_unlock(&ct);
-			pthread_mutex_lock(&tot);
-			total--;
-			pthread_mutex_unlock(&tot);
-		}
-		else{
-			v1_print_thread_result(info -> id, username, incomplete, count, time_diff, 1);
-			pthread_mutex_lock(&ct);
-			numFailed++;
-			pthread_mutex_unlock(&ct);
-			pthread_mutex_lock(&tot);
-			total--;
-			pthread_mutex_unlock(&tot);
-		}
-		// free(line);
-		// free(username);
-		// free(result);
+		incrementString(first_dot);
 	}
+	// printf("id: %zu out of loop\n", info -> id);
+	tot_ct += i;
+	cputime += getThreadCPUTime();
+	if(found){
+		v2_print_thread_result(info -> id, i, 0);
+		res = info -> start;
+	}
+	else if (i == (size_t)((info -> max))){
+		v2_print_thread_result(info -> id, i, 2);
+	}
+	else v2_print_thread_result(info -> id, i, 1);
+
 	return 0;
 }
 
-*/
+
 int start(size_t thread_count) {
 
-	pthread_mutex_init(&ct, 0);
+	pthread_mutex_init(&q, 0);
 	pthread_mutex_init(&tot, 0);
-	queue_t *jobs = queue_create(-1); //maybe -1 for unbounded?
 	int status = 0;
 	size_t length = 0;
 	char *line = NULL;
 	pthread_t **threads = malloc(thread_count * sizeof(pthread_t *));
 	td *arguments = malloc(thread_count * sizeof(td));
+	struct timespec start, end;
 
 	while((status = getline(&line, &length, stdin)) != -1){
+
+		pthread_mutex_lock(&q);
+		quit = 0;
+		pthread_mutex_unlock(&q);
+
 		char *line_dup = strdup(line);
 		char *nl = strchr(line_dup, '\n');
 		if (nl) *nl = 0;
-		queue_push(jobs, line_dup);
+
+		char *username = strsep(&line_dup, " "); // username
+		char *result = strsep(&line_dup, " "); // hashed password
+		char *incomplete = line_dup; // part of password given
+
+		size_t num_dots = strlen(incomplete) - getPrefixLength(incomplete);
+
+		// add subrange to struct
+		v2_print_start_user(username);
+		clock_gettime(CLOCK_MONOTONIC, &start);
+
+		for(size_t i = 0; i < thread_count; i++){
+			threads[i] = malloc(sizeof(pthread_t));
+			(arguments[i]).target = result;
+			(arguments[i]).id = i + 1;
+			(arguments[i]).user = username;
+			(arguments[i]).start = strdup(incomplete);
+
+			getSubrange(num_dots, thread_count, i + 1, &(arguments[i]).offset, &(arguments[i]).max);
+			pthread_create((threads[i]), 0, (void *)start_routine, &(arguments[i]));
+		}
 		total++;
-	}
-	for(size_t i = 0; i < thread_count; i++){
-		threads[i] = malloc(sizeof(pthread_t));
-		(arguments[i]).queue = jobs;
-		(arguments[i]).id = i;
-		pthread_create((threads[i]), 0, (void *)start_routine, &(arguments[i]));
-		queue_push(jobs, NULL);
-	}
-	quit = 1;
-	for(size_t i = 0; i < thread_count; i++){
-		pthread_join(*(threads[i]), 0);
-		free(threads[i]);
-	}
+		for(size_t i = 0; i < thread_count; i++){
+			pthread_join(*(threads[i]), 0);
+			free(threads[i]);
+		}
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		double time_diff = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec)/(1000000000);
+		v2_print_summary(username, res, tot_ct, time_diff, cputime, !quit);
+		cputime = 0;
+		tot_ct = 0;
+	} // end while
 	free(threads);
 	free(arguments);
-	queue_destroy(jobs);
-	v2_print_summary(numRecovered, numFailed);
 	return 0;
 }
