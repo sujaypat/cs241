29b1a4a423705058298c82966f242a726977bc40
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-11-02 18:21:18 +0000

    I have no fear

--- scheduler/libscheduler.c
+++ scheduler/libscheduler.c
@@ -19,7 +19,7 @@ struct _job_t {
 	float length;
 	float start;
 	float arrival;
-	// float remain;
+	float remain;
 	/* Add whatever other bookkeeping you need into this struct. */
 };
 
@@ -39,11 +39,10 @@ int num_procs = 0;
 
 int comparer_fcfs(const void *a, const void *b) {
 	int diff = ((job_t *)a) -> arrival - ((job_t *)b) -> arrival;
-	return (diff > 0) ? -1 : ((diff < 0) ? 1 : 0);
+	return (diff > 0) ? 1 : ((diff < 0) ? -1 : 0);
 }
 
 int break_tie(const void *a, const void *b) {
-
 	return comparer_fcfs(a, b);
 }
 
@@ -59,25 +58,23 @@ int comparer_pri(const void *a, const void *b) {
 }
 
 int comparer_psjf(const void *a, const void *b) {
-	// int diff = ((job_t *)a) -> remain_length - ((job_t *)b) -> remain_length;
-	// return (diff > 0) ? -1 : ((diff < 0) ? 1 : 0);
-	return comparer_sjf(a, b);
+	int diff = ((job_t *)a) -> remain - ((job_t *)b) -> remain;
+	if (diff == 0) return break_tie(a,b);
+	return (diff > 0) ? 1 : -1;
 }
 
 int comparer_rr(const void *a, const void *b) {
 	// Casting to void to silence compiler warning
 	(void)a;
 	(void)b;
-	// int r = rand();
 	// Picking 1 arbitarily.
 	return 1;
-	// return r > RAND_MAX/2 ? ((job_t *)a) -> id : ((job_t *)b) -> id;
 }
 
 int comparer_sjf(const void *a, const void *b) {
 	int diff = ((job_t *)a) -> length - ((job_t *)b) -> length;
 	if(diff == 0) return break_tie(a, b);
-	return (diff > 0) ? 1 : ((diff < 0) ? -1 : 0);
+	return (diff > 0) ? 1 : -1;
 }
 
 void scheduler_start_up(int numcores, scheme_t s) {
@@ -127,6 +124,7 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority) 
 	j -> arrival = time;
 	j -> length = running_time;
 	j -> start = 0;
+	j -> remain = j -> length;
 
 	num_procs++;
 
@@ -151,31 +149,36 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority) 
 			cores[i].job = j;
 			return i;
 		}
-		if(preempt == 0){
-			if(comparer_ppri(j, cores[i].job) == -1){
-				priqueue_offer(&pqueue, cores[i].job);
-				cores[i].free = 0;
-				j -> start = time;
-				return i;
-			}
-		}
-		if(preempt == 1){
-			cores[i].job -> length -= (time - cores[i].job -> start);
-			if(comparer_psjf(j, cores[i].job) == -1){
-				priqueue_offer(&pqueue, cores[i].job);
-				cores[i].free = 0;
-				j -> start = time;
-				return i;
+		cores[i].job -> remain -= (time - cores[i].job -> start);
+	}
+	priqueue_offer(&pqueue, j);
+	job_t *new = priqueue_poll(&pqueue);
+	if(preempt == 0){
+		for (size_t k = 0; k < num_cores; k++) {
+			if(comparer_ppri(new, cores[k].job) == -1){
+				priqueue_offer(&pqueue, cores[k].job);
+				cores[k].free = 0;
+				new -> start = time;
+				return k;
 			}
 		}
 	}
-	priqueue_offer(&pqueue, j);
+	if(preempt == 1){
+		for (size_t k = 0; k < num_cores; k++) {
+			if(comparer_psjf(new, cores[k].job) == -1){
+				priqueue_offer(&pqueue, cores[k].job);
+				cores[k].free = 0;
+				new -> start = time;
+				return k;
+			}
+		}
+	}
+	priqueue_offer(&pqueue, new);
 	return -1;
 }
 
 int scheduler_job_finished(int core_id, int job_number, int time) {
 
-	printf("waiting: %f\n", (time - cores[core_id].job -> arrival) - cores[core_id].job -> length);
 	waiting += (time - cores[core_id].job -> arrival) - cores[core_id].job -> length;
 	job_t *job = (job_t *)(priqueue_poll(&pqueue));
 	if(!job){
