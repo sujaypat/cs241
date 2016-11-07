6ec9b1f9eaa013573941a69aade245dbd5d22818
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-07 02:26:03 +0000

    mvp

--- ptwrdhn2/parmake/parmake.c
+++ ptwrdhn2/parmake/parmake.c
@@ -21,8 +21,12 @@
 int num_threads = 1;
 char *path = NULL;
 Vector *rules;
+Vector *satisfied;
 queue_t *jobs;
 pthread_mutex_t vec;
+size_t num_jobs = 0;
+
+// state: 0-unprocessed, 1-added, 2-completed, 3-failed
 
 void *my_copy_ctor(void *elem) {
 	return elem;
@@ -31,31 +35,56 @@ void my_destructor(void *elem) {
 	elem = (rule_t *)elem;
 }
 
+int contains(Vector *search, rule_t *in){
+	int res = 0;
+	for(size_t i = 0; i < Vector_size(search); i++){
+		// printf("comparing %s with %s\n", in -> target, ((rule_t *)Vector_get(search, i)) -> target);
+		if(!strcmp(in -> target, ((rule_t *)Vector_get(search, i)) -> target)){
+			res = 1;
+		}
+	}
+	return res;
+}
+
+void print_vector(Vector *in){
+	for (size_t i = 0; i < Vector_size(in); i++) {
+		fprintf(stderr, "%s ", ((rule_t *)Vector_get(in, i)) -> target);
+	}
+	fprintf(stderr, "\n");
+}
+
 void callback(rule_t *target){
-	printf("rule target added to vector: %s\n", target -> target);
 	Vector_append(rules, target);
 }
 
 void * start_routine(void *input){
 	queue_t *use = (queue_t *)input;
 	while(1){
+		int flag = 0;
 		rule_t *thread_rule = queue_pull(use);
 		if(!thread_rule) break;
-		if(thread_rule -> state == 0){
-			// int all_satisfied = 0;
-			for(size_t j = 0; j < Vector_size(thread_rule -> dependencies); j++){
-				if(Vector_get(thread_rule -> dependencies, j) == Vector_get(rules, i)){
-					Vector_delete(thread_rule -> dependencies, i);
+
+		for(size_t j = 0; j < Vector_size(thread_rule -> dependencies); j++){
+			while(((rule_t *)Vector_get(thread_rule -> dependencies, j)) -> state != 2 && ((rule_t *)Vector_get(thread_rule -> dependencies, j)) -> state != 3){}
+			if(((rule_t *)Vector_get(thread_rule -> dependencies, j)) -> state == 3){
+				flag = 1;
+			}
+		}
+		if(!flag){
+			for(size_t i = 0; i < Vector_size(thread_rule -> commands); i++){
+				int out = system(Vector_get(thread_rule -> commands, i));
+				if(out != 0){
+					thread_rule -> state = 3;
+					continue;
 				}
 			}
-			if(!Vector_size(thread_rule -> dependencies)){
-				thread_rule -> state = 1; // complete
-				queue_push(jobs, thread_rule);
-			}
+			thread_rule -> state = 2;
 		}
 	}
 	return 0;
 }
+
+
 
 int parmake(int argc, char **argv) {
 
@@ -90,18 +119,51 @@ int parmake(int argc, char **argv) {
 	}
 
 	rules = Vector_create(&my_copy_ctor, &my_destructor);
+	satisfied = Vector_create(&my_copy_ctor, &my_destructor);
 	jobs = queue_create(-1, &my_copy_ctor, &my_destructor);
 
 	parser_parse_makefile(path, &argv[optind], callback);
+
 
 	for (size_t i = 0; i < Vector_size(rules); i++) {
 		rule_t *temp = Vector_get(rules, i);
 		if(Vector_size(temp -> dependencies) == 0){
 			((rule_t *)(Vector_get(rules, i))) -> state = 1;
-			printf("rule pushed to queue: %s\n", temp -> target);
+			// printf("added to queue with 0 dependencies: %s\n", ((rule_t *)(Vector_get(rules, i))) -> target);
 			queue_push(jobs, temp);
+			num_jobs++;
+			Vector_append(satisfied, temp);
+			// print_vector(satisfied);
 		}
-		Vector_delete(rules, i);
+	}
+	size_t total_rules = Vector_size(rules);
+	int flag = 0; // 0 if found, 1 if not found
+	while(total_rules > 0){
+		size_t orig_num = total_rules;
+		for(size_t i = 0; i < Vector_size(rules); i++){
+			flag = 0;
+			for(size_t j = 0; j < Vector_size(((rule_t *)Vector_get(rules, i)) -> dependencies); j++){
+				// printf("current rule: %s\n", ((rule_t *)Vector_get(rules, i)) -> target);
+				// printf("searching for %s in %s\n", ((rule_t *)Vector_get(((rule_t *)Vector_get(rules, i)) -> dependencies, j)) -> target, "satisfied");
+				if(!contains(satisfied, Vector_get(((rule_t *)Vector_get(rules, i)) -> dependencies, j))){
+					// printf("not found\n");
+					flag = 1;
+					break;
+				}
+			}
+			if(!flag && !contains(satisfied, ((rule_t *)Vector_get(rules, i)))){
+				// printf("adding to queue: %s\n", ((rule_t *)(Vector_get(rules, i))) -> target);
+				num_jobs++;
+				queue_push(jobs, Vector_get(rules, i));
+				Vector_append(satisfied, Vector_get(rules, i));
+				// print_vector(satisfied);
+				total_rules--;
+			}
+		}
+		if(orig_num == total_rules){
+			// cycle detected
+			break;
+		}
 	}
 
 	for(int j = 0; j < num_threads; j++){
