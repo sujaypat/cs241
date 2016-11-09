fd755d90364e8c41fd794c7b2027985f86ba9641
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-09 10:06:38 +0000

    aids

--- ptwrdhn2/finding_filesystems/fs.c
+++ ptwrdhn2/finding_filesystems/fs.c
@@ -17,11 +17,37 @@
 
 void fs_ls(file_system *fs, char *path) {
 	// Arrrrrgh Matey
-	// superblock *sblock = fs -> meta;
-	// inode *root = fs -> inode_root;
-	// data_block *first_data = fs -> data_root;
+	inode *res = NULL;
+	dirent dir;
+	if(!(res = get_inode(fs, path))){
+		print_no_file_or_directory();
+		return;
+	}
+	if(is_file(res)){
+		char *file = NULL;
+		parent_directory(fs, path, &file);
+		print_file(file);
+	}
+	else if(is_directory(res)){
+		int num_entries = res -> size / 256;
+		for(size_t i = 0; i < 11; i++){
+			for (size_t j = 0; j < 64; j++) {
+				char *same = (char *)(((void *)(&(fs -> data_root[res -> direct_nodes[i]]))) + 256 * j);
+				make_dirent_from_string(same, &dir);
 
-	printf("%s\n", path);
+				inode *file_or_dir = &(fs -> inode_root[dir.inode_num]);
+				if(is_file(file_or_dir)) print_file(dir.name);
+				else if(is_directory(file_or_dir)) print_directory(dir.name);
+				else{
+					print_no_file_or_directory();
+					return;
+				}
+				// printf("num_entries: %d\n", num_entries);
+				num_entries--;
+				if(num_entries == 0) return;
+			}
+		}
+	}
 }
 
 void fs_cat(file_system *fs, char *path) {
