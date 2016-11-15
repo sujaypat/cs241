6863be99dece9ebbe395099dffd85b27579e5ec3
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-15 23:43:36 +0000

    not working

--- ptwrdhn2/mad_mad_access_patterns/lookup1.c
+++ ptwrdhn2/mad_mad_access_patterns/lookup1.c
@@ -1,14 +1,96 @@
 /**
- * Mad Mad Access Pattern
- * CS 241 - Fall 2016
- */
+* Mad Mad Access Pattern
+* CS 241 - Fall 2016
+*/
 #include "tree.h"
 #include "utils.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 /*
-  Look up a few nodes in the tree and print the info they contain.
-  This version uses fseek() and fread() to access the data.
+Look up a few nodes in the tree and print the info they contain.
+This version uses fseek() and fread() to access the data.
 
-  ./lookup1 <data_file> <word> [<word> ...]
+./lookup1 <data_file> <word> [<word> ...]
 */
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+	// argv[1] = filename, argv[2] = keyword, argv[3+] = optional more keywords
+	if(argc < 3){
+		printArgumentUsage();
+		exit(1);
+	}
+	FILE *f = fopen(argv[1], "r");
+	if(!f){
+		openFail(argv[1]);
+		exit(2);
+	}
+	char *first_four = calloc(5, 1);
+	fread(first_four, BINTREE_ROOT_NODE_OFFSET, 1, f);
+	if(strcmp(BINTREE_HEADER_STRING, first_four)){
+		formatFail(argv[1]);
+		exit(2);
+	}
+
+	for(int i = 2; i < argc; i++){
+		fseek(f, BINTREE_ROOT_NODE_OFFSET + sizeof(BinaryTreeNode), SEEK_SET);
+		int terminator = 0;
+		while(1){
+			// first char of word = curr + 16
+			char temp = fgetc(f);
+			terminator++;
+			if(temp == '\0') break;
+		}
+
+		BinaryTreeNode *temp = calloc(sizeof(BinaryTreeNode) + terminator, 1);
+		fseek(f, BINTREE_ROOT_NODE_OFFSET, SEEK_SET);
+		fread(temp, sizeof(BinaryTreeNode) + terminator, 1, f);
+
+		int flag = 0;
+
+		while(1){
+			int comp = strcmp(temp -> word, argv[i]);
+			if(!comp){
+				printFound(argv[i], temp -> count, temp -> price);
+				flag = 1;
+				free(temp);
+				break;
+			}
+			else if(comp < 0){
+				if(temp -> right_child == 0) break;
+				fseek(f, temp -> right_child + sizeof(BinaryTreeNode), SEEK_SET);
+				int terminator = 0;
+				while(1){
+					// first char of word = curr + 16
+					char temp = fgetc(f);
+					terminator++;
+					if(temp == '\0') break;
+				}
+				fseek(f, temp -> right_child, SEEK_SET);
+				fread(temp, sizeof(BinaryTreeNode) + terminator, 1, f);
+			}
+			else{
+				if(temp -> left_child == 0) break;
+				fseek(f, temp -> left_child + sizeof(BinaryTreeNode), SEEK_SET);
+				int terminator = 0;
+				while(1){
+					// first char of word = curr + 16
+					char temp = fgetc(f);
+					terminator++;
+					if(temp == '\0') break;
+				}
+				fseek(f, temp -> left_child, SEEK_SET);
+				fread(temp, sizeof(BinaryTreeNode) + terminator, 1, f);
+			}
+		}
+		if (!flag){
+			printNotFound(argv[i]);
+			if(temp) free(temp);
+		}
+
+	}
+	free(first_four);
+	fclose(f);
+	return 0;
+}
--- ptwrdhn2/mad_mad_access_patterns/lookup2.c
+++ ptwrdhn2/mad_mad_access_patterns/lookup2.c
@@ -1,15 +1,75 @@
 /**
- * Mad Mad Access Pattern
- * CS 241 - Fall 2016
- */
+* Mad Mad Access Pattern
+* CS 241 - Fall 2016
+*/
 #include "tree.h"
 #include "utils.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 /*
-  Look up a few nodes in the tree and print the info they contain.
-  This version uses mmap to access the data.
+Look up a few nodes in the tree and print the info they contain.
+This version uses mmap to access the data.
 
-  ./lookup2 <data_file> <word> [<word> ...]
+./lookup2 <data_file> <word> [<word> ...]
 */
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+	// argv[1] = filename, argv[2] = keyword, argv[3+] = optional more keywords
+	if(argc < 3){
+		printArgumentUsage();
+		exit(1);
+	}
+	struct stat statinfo;
+	int staterr = stat(argv[1], &statinfo);
+	if(staterr == -1){
+		openFail(argv[1]);
+		exit(1);
+	}
+	size_t filesize = statinfo.st_size;
+	int openfile = open(argv[1], O_RDONLY);
+	void *addr = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, openfile, 0);
+	if(!addr){
+		openFail(argv[1]);
+		exit(1);
+	}
+	if(strncmp((char *)addr, BINTREE_HEADER_STRING, 4) != 0){
+		formatFail(argv[1]);
+		exit(2);
+	}
+	for(int i = 2; i < argc; i++){
+
+		BinaryTreeNode *temp = addr + BINTREE_ROOT_NODE_OFFSET;
+		int flag = 0;
+		while(1){
+			int comp = strcmp(temp -> word, argv[i]);
+			if(!comp){
+				printFound(argv[i], temp -> count, temp -> price);
+				flag = 1;
+				break;
+			}
+			else if(comp < 0){
+				if(temp -> right_child == 0) break;
+				temp = addr + temp -> right_child;
+			}
+			else{
+				if(temp -> left_child == 0) break;
+				temp = addr + temp -> left_child;
+			}
+		}
+		if (!flag){
+			printNotFound(argv[i]);
+		}
+
+	}
+	int munmaperr = munmap(addr, filesize);
+	if(munmaperr == -1){
+		printf("rip\n");
+	}
+	return 0;
+}
