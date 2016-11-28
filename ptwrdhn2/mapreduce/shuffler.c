f4efaae97b6a65331e3f51b6274033dfe6c7b079
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-11-28 06:12:43 +0000

    done

--- ptwrdhn2/mapreduce/shuffler.c
+++ ptwrdhn2/mapreduce/shuffler.c
@@ -21,15 +21,39 @@ int main(int argc, char *argv[]) {
 	// hash the key for the input line
 	// send them to the correct output file (output files are given as command
 	// line arguments
+	FILE *output_files[argc - 1];
+	for(int i = 1; i < argc; i++){
+		//printf("%s\n", argv[i]);
+		output_files[i - 1] = fopen(argv[i], "w");
+	}
+	size_t N = argc - 1;
 	if (argc < 2) {
 		usage();
 		exit(1);
 	}
-	while(getline() != -1){
-		char *key = ;
-		char *value = ;
+	char *line = NULL;
+	size_t len = 0;
+	while(getline(&line, &len, stdin) != EOF){
+		//fprintf(stderr, "reading line %s\n", line);
+		char *key = NULL;
+		char *value = NULL;
+		split_key_value(line, &key, &value);
 		FILE *outf = output_files[hashKey(key) % N];
-		fprintf(outf, "%s: %s\n", key, value)
+
+		//fprintf(stderr, "%s: %s\n", key, value);
+
+		fprintf(outf, "%s: %s\n", key, value);
+		//fprintf(stderr,"i just sent that shit straight to the fifo\n");
+		line = NULL;
+		len = 0;
+		
 	}
+	//if(line) free(line);
+	
+//	for(int i = 1; i < argc; i++){
+		//printf("%s\n", argv[i]);
+//		fclose(output_files[i]);
+//	}
+	//fprintf(stderr,"exiting the shuffler\n");
 	return 0;
 }
