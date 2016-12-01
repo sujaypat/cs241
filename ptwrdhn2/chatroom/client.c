b78bacf8a6c91ff7ba5ee7f624717f33831c6d8f
ptwrdhn2 <ptwrdhn2@9ec08a75-e2be-4a27-adf6-5ed6d76ab7fa>
2016-12-01 04:22:48 +0000

    rip kids

--- ptwrdhn2/chatroom/client.c
+++ ptwrdhn2/chatroom/client.c
@@ -1,7 +1,7 @@
 /**
- * Chatroom Lab
- * CS 241 - Fall 2016
- */
+* Chatroom Lab
+* CS 241 - Fall 2016
+*/
 
 #include <errno.h>
 #include <netdb.h>
@@ -25,165 +25,178 @@ void *read_from_server(void *arg);
 void close_program(int signal);
 
 /**
- * Clean up for client
- * Called by close_program upon SIGINT
- */
+* Clean up for client
+* Called by close_program upon SIGINT
+*/
 void close_client() {
-  // Cancel the running threads
-  pthread_cancel(threads[0]);
-  pthread_cancel(threads[1]);
-
-  // Any other cleanup code goes here!
+	// Cancel the running threads
+	pthread_cancel(threads[0]);
+	pthread_cancel(threads[1]);
+	// Any other cleanup code goes here!
 }
 
 /**
- * Sets up a connection to a chatroom server and begins
- * reading and writing to the server.
- *
- * host     - Server to connect to.
- * port     - Port to connect to server on.
- * username - Name this user has chosen to connect to the chatroom with.
- */
+* Sets up a connection to a chatroom server and begins
+* reading and writing to the server.
+*
+* host     - Server to connect to.
+* port     - Port to connect to server on.
+* username - Name this user has chosen to connect to the chatroom with.
+*/
 void run_client(const char *host, const char *port, const char *username) {
-  /*QUESTION 1*/
-  /*QUESTION 2*/
-  /*QUESTION 3*/
+	signal(SIGINT, close_program);
 
-  /*QUESTION 4*/
-  /*QUESTION 5*/
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0); /* create socket */
 
-  /*QUESTION 6*/
+	struct addrinfo client, *res;
+	// socklen_t clientAddrlen = sizeof(struct addrinfo);
+	memset(&client, 0, sizeof(struct addrinfo));
+	client.ai_family = AF_INET;
+	client.ai_socktype = SOCK_STREAM;
 
-  /*QUESTION 7*/
+	int s;
+	s = getaddrinfo(host, port, &client, &res);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(1);
+	}
+	connect(serverSocket, res->ai_addr, res->ai_addrlen);
+
+	pthread_create(&threads[0], 0, write_to_server, (void *)username);
+	pthread_create(&threads[1], 0, read_from_server, 0);
+
+	pthread_join(threads[0], 0);
+	pthread_join(threads[1], 0);
+	free(res);
 }
 
 typedef struct _thread_cancel_args {
-  char **buffer;
-  char **msg;
+	char **buffer;
+	char **msg;
 } thread_cancel_args;
 
 /**
- * Cleanup routine in case the thread gets cancelled
- * Ensure buffers are freed if they point to valid memory
- */
+* Cleanup routine in case the thread gets cancelled
+* Ensure buffers are freed if they point to valid memory
+*/
 void thread_cancellation_handler(void *arg) {
-  printf("Cancellation handler\n");
-  thread_cancel_args *a = (thread_cancel_args *)arg;
-  char **msg = a->msg;
-  char **buffer = a->buffer;
-  if (*buffer) {
-    free(*buffer);
-    *buffer = NULL;
-  }
-  if (msg && *msg) {
-    free(*msg);
-    *msg = NULL;
-  }
+	printf("Cancellation handler\n");
+	thread_cancel_args *a = (thread_cancel_args *)arg;
+	char **msg = a->msg;
+	char **buffer = a->buffer;
+	if (*buffer) {
+		free(*buffer);
+		*buffer = NULL;
+	}
+	if (msg && *msg) {
+		free(*msg);
+		*msg = NULL;
+	}
 }
 
 /**
- * Reads bytes from user and writes them to server
- *
- * arg - void* casting of char* that is the username of client
- */
+* Reads bytes from user and writes them to server
+*
+* arg - void* casting of char* that is the username of client
+*/
 void *write_to_server(void *arg) {
-  char *name = (char *)arg;
-  char *buffer = NULL;
-  char *msg = NULL;
-  ssize_t retval = 1;
+	char *name = (char *)arg;
+	char *buffer = NULL;
+	char *msg = NULL;
+	ssize_t retval = 1;
 
-  thread_cancel_args cancel_args;
-  cancel_args.buffer = &buffer;
-  cancel_args.msg = &msg;
-  // Setup thread cancellation handlers
-  // Read up on pthread_cancel, thread cancellation states, pthread_cleanup_push
-  // for more!
-  pthread_cleanup_push(thread_cancellation_handler, &cancel_args);
+	thread_cancel_args cancel_args;
+	cancel_args.buffer = &buffer;
+	cancel_args.msg = &msg;
+	// Setup thread cancellation handlers
+	// Read up on pthread_cancel, thread cancellation states, pthread_cleanup_push
+	// for more!
+	pthread_cleanup_push(thread_cancellation_handler, &cancel_args);
 
-  while (retval > 0) {
-    read_message_from_screen(&buffer);
-    if (buffer == NULL)
-      break;
+	while (retval > 0) {
+		read_message_from_screen(&buffer);
+		if (buffer == NULL)
+		break;
 
-    msg = create_message(name, buffer);
-    size_t len = strlen(msg) + 1;
+		msg = create_message(name, buffer);
+		size_t len = strlen(msg) + 1;
 
-    retval = write_message_size(len, serverSocket);
-    if (retval > 0)
-      retval = write_all_to_socket(serverSocket, msg, len);
+		retval = write_message_size(len, serverSocket);
+		if (retval > 0)
+		retval = write_all_to_socket(serverSocket, msg, len);
 
-    free(msg);
-    msg = NULL;
-  }
+		free(msg);
+		msg = NULL;
+	}
 
-  pthread_cleanup_pop(0);
-  return 0;
+	pthread_cleanup_pop(0);
+	return 0;
 }
 
 /**
- * Reads bytes from the server and prints them to the user.
- *
- * arg - void* requriment for pthread_create function
- */
+* Reads bytes from the server and prints them to the user.
+*
+* arg - void* requriment for pthread_create function
+*/
 void *read_from_server(void *arg) {
-  // Silence the unused parameter warning
-  (void)arg;
-  ssize_t retval = 1;
-  char *buffer = NULL;
-  thread_cancel_args cancellation_args;
-  cancellation_args.buffer = &buffer;
-  cancellation_args.msg = NULL;
-  pthread_cleanup_push(thread_cancellation_handler, &cancellation_args);
+	// Silence the unused parameter warning
+	(void)arg;
+	ssize_t retval = 1;
+	char *buffer = NULL;
+	thread_cancel_args cancellation_args;
+	cancellation_args.buffer = &buffer;
+	cancellation_args.msg = NULL;
+	pthread_cleanup_push(thread_cancellation_handler, &cancellation_args);
 
-  while (retval > 0) {
-    retval = get_message_size(serverSocket);
-    if (retval > 0) {
-      buffer = calloc(1, retval);
-      retval = read_all_from_socket(serverSocket, buffer, retval);
-    }
-    if (retval > 0)
-      write_message_to_screen("%s\n", buffer);
+	while (retval > 0) {
+		retval = get_message_size(serverSocket);
+		if (retval > 0) {
+			buffer = calloc(1, retval);
+			retval = read_all_from_socket(serverSocket, buffer, retval);
+		}
+		if (retval > 0)
+		write_message_to_screen("%s\n", buffer);
 
-    free(buffer);
-    buffer = NULL;
-  }
+		free(buffer);
+		buffer = NULL;
+	}
 
-  pthread_cleanup_pop(0);
-  return 0;
+	pthread_cleanup_pop(0);
+	return 0;
 }
 
 /**
- * Signal handler used to close this client program.
- */
+* Signal handler used to close this client program.
+*/
 void close_program(int signal) {
-  if (signal == SIGINT) {
-    close_chat();
-    close_client();
-  }
+	if (signal == SIGINT) {
+		close_chat();
+		close_client();
+	}
 }
 
 int main(int argc, char **argv) {
 
-  if (argc < 4 || argc > 5) {
-    fprintf(stderr, "Usage: %s <address> <port> <username> [output_file]\n",
-            argv[0]);
-    exit(1);
-  }
+	if (argc < 4 || argc > 5) {
+		fprintf(stderr, "Usage: %s <address> <port> <username> [output_file]\n",
+		argv[0]);
+		exit(1);
+	}
 
-  char *output_filename;
-  if (argc == 5) {
-    output_filename = argv[4];
-  } else {
-    output_filename = NULL;
-  }
+	char *output_filename;
+	if (argc == 5) {
+		output_filename = argv[4];
+	} else {
+		output_filename = NULL;
+	}
 
-  // Setup signal handler
-  signal(SIGINT, close_program);
-  create_windows(output_filename);
+	// Setup signal handler
+	signal(SIGINT, close_program);
+	create_windows(output_filename);
 
-  run_client(argv[1], argv[2], argv[3]);
+	run_client(argv[1], argv[2], argv[3]);
 
-  destroy_windows();
+	destroy_windows();
 
-  return 0;
+	return 0;
 }
--- ptwrdhn2/chatroom/utils.c
+++ ptwrdhn2/chatroom/utils.c
@@ -1,43 +1,68 @@
 /**
- * Chatroom Lab
- * CS 241 - Fall 2016
- */
+* Chatroom Lab
+* CS 241 - Fall 2016
+*/
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
 #include "utils.h"
 static const size_t MESSAGE_SIZE_DIGITS = 4;
 
 char *create_message(char *name, char *message) {
-  int name_len = strlen(name);
-  int msg_len = strlen(message);
-  char *msg = calloc(1, msg_len + name_len + 4);
-  sprintf(msg, "%s: %s", name, message);
+	int name_len = strlen(name);
+	int msg_len = strlen(message);
+	char *msg = calloc(1, msg_len + name_len + 4);
+	sprintf(msg, "%s: %s", name, message);
 
-  return msg;
+	return msg;
 }
 
 ssize_t get_message_size(int socket) {
-  int32_t size;
-  ssize_t read_bytes =
-      read_all_from_socket(socket, (char *)&size, MESSAGE_SIZE_DIGITS);
-  if (read_bytes == 0 || read_bytes == -1)
-    return read_bytes;
+	int32_t size;
+	ssize_t read_bytes = read_all_from_socket(socket, (char *)&size, MESSAGE_SIZE_DIGITS);
+	if (read_bytes == 0 || read_bytes == -1) return read_bytes;
 
-  return (ssize_t)ntohl(size);
+	return (ssize_t)ntohl(size);
 }
 
 ssize_t write_message_size(size_t size, int socket) {
-  // Your code here
-  return 9001;
+	ssize_t res;
+	ssize_t payload = (ssize_t)htonl(size);
+
+	res = write_all_to_socket(socket, (char *)&(payload), (MESSAGE_SIZE_DIGITS));
+
+	return res;
 }
 
 ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
-  // Your Code Here
-  return 9001;
+	ssize_t res;
+	size_t progress = 0;
+	while(progress != count){
+		res = read(socket, buffer + progress, count - progress);
+		if(res != -1) progress += res;
+		else{
+			if(res == -1 && errno == EINTR) continue;
+			if((res == -1 && errno != EINTR)) return -1;
+		}
+	}
+	return res;
 }
 
 ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
-  // Your Code Here
-  return 9001;
+	ssize_t res;
+	size_t progress = 0;
+	// fprintf(stderr, "buffer to write back: %s\n", buffer);
+	while(progress != count){
+		res = write(socket, buffer + progress, count - progress);
+		if(res != -1) progress += res;
+		else{
+			if(res == -1 && errno == EINTR) continue;
+			if((res == -1 && errno != EINTR)) return -1;
+		}
+	}
+	return res;
 }
